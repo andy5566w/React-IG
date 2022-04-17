e8eedb27635ebf9835585a31e2a2cd4dd7cee228
andy.tsai <andy.tsai@deephow.com>
2022-04-17 21:14:21 +0800

    fix input props

--- src/components/UI/form/Input.js
+++ src/components/UI/form/Input.js
@@ -1,15 +1,15 @@
-import classes from "./Input.module.scss";
+import classes from './Input.module.scss'
 const Input = (props) => {
-  const { type, label, value, handleValue } = props;
-  console.log(classes);
+  const { type, label, value, handleValue, styleName } = props
+  console.log(classes)
   return (
-    <div className={classes["input-div"]}>
+    <div className={`${classes['input-div']} ${value ? styleName : ''}`}>
       <label>
-        <span className={`${value && classes["span-move"]}`}>{label}</span>
-        <input type={type || "text"} value={value} onChange={handleValue} />
+        <span className={`${value && classes['span-move']}`}>{label}</span>
+        <input type={type || 'text'} value={value} onChange={handleValue} />
       </label>
     </div>
-  );
-};
+  )
+}
 
-export default Input;
+export default Input
