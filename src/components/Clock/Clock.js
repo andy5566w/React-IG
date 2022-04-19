735934cc7b04df30e15f898e9dd0bad4b52a133f
andy.tsai <andy.tsai@deephow.com>
2022-04-19 22:29:04 +0800

    add clock js login.

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,8 +1,44 @@
 import classes from './Clock.module.scss'
+import { useState, useCallback } from 'react'
+
 const Clock = () => {
+  const day = new Date()
+  const [hrDegree, setHrDegree] = useState(day.getHours() * 30)
+  const [mnDegree, setMnDegree] = useState(day.getMinutes() * 6)
+  const [scDegree, setScDegree] = useState(day.getSeconds() * 6)
+  const handleCalculateDegree = useCallback(() => {
+    const day = new Date()
+    setHrDegree(() => day.getHours() * 30)
+    setMnDegree(() => day.getMinutes() * 6)
+    setScDegree(() => day.getSeconds() * 6)
+  }, [setHrDegree, setMnDegree, setScDegree])
+
+  setInterval(() => {
+    handleCalculateDegree()
+  }, 1000)
+
   return (
     <div className={classes.container}>
       <div className={classes.clock}>
+        <div
+          style={{ '--color': '#ff2972', transform: `rotate(${hrDegree}deg)` }}
+          className={`${classes.circle}`}
+        >
+          <i></i>
+        </div>
+        <div
+          style={{ '--color': '#fee800', transform: `rotate(${mnDegree}deg)` }}
+          className={`${classes.circle} ${classes.circle2}`}
+        >
+          <i></i>
+        </div>
+        <div
+          style={{ '--color': '#04fc43', transform: `rotate(${scDegree}deg)` }}
+          className={`${classes.circle} ${classes.circle3}`}
+        >
+          <i></i>
+        </div>
+
         <span style={{ '--i': 1 }}>{<b>1</b>}</span>
         <span style={{ '--i': 2 }}>{<b>2</b>}</span>
         <span style={{ '--i': 3 }}>{<b>3</b>}</span>
